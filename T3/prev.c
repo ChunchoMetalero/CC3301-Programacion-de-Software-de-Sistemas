b6e7d15ef82d3b2559bba74e4f18575bc8f7605d
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2023-09-29 05:25:06 -0300

    Tarea 3 Completa

--- T3/prev.c
+++ T3/prev.c
@@ -2,6 +2,19 @@
 
 #include "prev.h"
 
+
 void asignarPrev(Nodo *t, Nodo **pprev) {
-  ...
+  if (t == NULL) return;
+  else {
+    asignarPrev(t->izq, pprev); 
+    t->prev = *pprev; 
+    if(*pprev != NULL) { 
+      (*pprev)->prox = t; 
+    } 
+    *pprev = t; 
+    if (*pprev == t) { 
+      t->prox = NULL; 
+    }
+    asignarPrev(t->der, pprev); 
+  }
 }
